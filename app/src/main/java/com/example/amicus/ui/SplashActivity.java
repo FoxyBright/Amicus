8ee2b8b7f7b373461ab768a90262558f0eaecd88
FoxyBright <VANLEG@yandex.ru>
2021-11-25 09:59:14 +0300

    работает полохо

--- app/src/main/java/com/example/amicus/ui/SplashActivity.java
+++ app/src/main/java/com/example/amicus/ui/SplashActivity.java
@@ -1,6 +1,37 @@
-package com.example.amicus.ui;
-
-public class SplashActivity {
-
-
-}
+//package com.example.amicus.ui;
+//
+//import android.graphics.Typeface;
+//import android.os.Bundle;
+//import android.widget.ImageView;
+//import android.widget.ProgressBar;
+//import android.widget.TextView;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.constraintlayout.widget.ConstraintLayout;
+//import androidx.navigation.NavController;
+//import androidx.navigation.Navigation;
+//import androidx.navigation.ui.AppBarConfiguration;
+//import androidx.navigation.ui.NavigationUI;
+//
+//import com.example.amicus.R;
+//import com.example.amicus.databinding.ActivityMainBinding;
+//import com.google.android.material.bottomnavigation.BottomNavigationView;
+//
+//public class SplashActivity extends AppCompatActivity {
+//
+//    private ConstraintLayout layout;
+//    private ImageView image;
+//    private TextView text;
+//
+//    private static final int SPLASH_DURATION = 2500;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.splashscreen);
+//        layout = findViewById(R.id.splash);
+//    }
+//
+//
+//
+//}
