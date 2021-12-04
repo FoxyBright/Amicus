6dff7e33571111fa2db5814189cf6824a247406b
Veles <VANLEG@yandex.ru>
2021-12-04 21:15:58 +0300

    Добавлена форма настройки приложения

---
+++ app/src/main/java/com/example/amicus/ui/AppSettingsActivity.java
@@ -0,0 +1,15 @@
+package com.example.amicus.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.amicus.R;
+
+public class AppSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_settings);
+    }
+}
