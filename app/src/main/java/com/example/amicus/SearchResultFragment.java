b95c6519e006fb04acf35d8a9917dc47ea5854e7
Veles <VANLEG@yandex.ru>
2021-12-17 01:08:37 +0300

    Добавлена форма добавления поездки

---
+++ app/src/main/java/com/example/amicus/SearchResultFragment.java
@@ -0,0 +1,22 @@
+package com.example.amicus;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SearchResultFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_search_result, container, false);
+
+
+
+        return view;
+    }
+}
