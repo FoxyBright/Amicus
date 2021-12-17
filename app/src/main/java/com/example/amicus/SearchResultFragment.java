02e67d28f5de0de19a6cf375d497681243287187
Veles <VANLEG@yandex.ru>
2021-12-17 21:43:41 +0300

    Система почти завершена в макете

--- app/src/main/java/com/example/amicus/SearchResultFragment.java
+++ app/src/main/java/com/example/amicus/SearchResultFragment.java
@@ -7,13 +7,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.widget.Button;
 
 public class SearchResultFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_result, container, false);
+        View view = inflater.inflate(R.layout.fragment_search_result, container, false);
+        Button one_travel_bt = view.findViewById(R.id.one_travel_bt);
+        one_travel_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return view;
     }
 }
