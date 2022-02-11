3c6f592247b6bcd80d75700c47e2bc1dca6ecbab
tihv@automacon.ru <PokerStars1>
2022-02-12 02:36:00 +0300

    Регистрация с места

--- app/src/main/java/com/example/amicus/Registration.java
+++ app/src/main/java/com/example/amicus/Registration.java
@@ -5,32 +5,54 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
-import com.example.amicus.ui.ConfirmationActivity;
+import com.google.android.material.textfield.TextInputLayout;
+
 
 public class Registration extends AppCompatActivity {
 
-    private TextView enter_link;
-    private Button continue_bt;
+    EditText your_name_textview;
+    EditText number_textview;
+    EditText password_create;
+    EditText repeat_pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
+        your_name_textview = findViewById(R.id.your_name_textview);
+        number_textview = findViewById(R.id.login_email_et);
+        password_create = findViewById(R.id.login_password_et);
+        repeat_pass = findViewById(R.id.login_password_et_rewrite);
+        ToggleButton toggleButton1 = findViewById(R.id.toggleButton1);
         TextView enter_link = findViewById(R.id.enter_link);
         Button continue_bt = findViewById(R.id.continue_bt);
+
 
         continue_bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Registration.this, ConfirmationActivity.class);
-                startActivity(intent);
+                String name = your_name_textview.getText().toString();
+                String phone = number_textview.getText().toString();
+                String pass = password_create.getText().toString();
+                String rep_pass =password_create.getText().toString();
+                if (validateForm(password_create,repeat_pass) && toggleButton1.isChecked() && !TextUtils.isEmpty(name) && !TextUtils.isEmpty(phone)
+                &&!TextUtils.isEmpty(pass) &&!TextUtils.isEmpty(rep_pass)) {
+                    Intent intent = new Intent(Registration.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    Toast.makeText(Registration.this, "Что то мы забыли", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -42,20 +64,6 @@ public class Registration extends AppCompatActivity {
             }
         });
 
-        continue_bt.setClickable(false);
-
-        ToggleButton toggleButton1 = findViewById(R.id.toggleButton1);
-        toggleButton1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (toggleButton1.isChecked()){
-                    continue_bt.setClickable(true);
-                }
-                else{
-                    continue_bt.setClickable(false);
-                }
-            }
-        });
 
         policy_of_conf.setPaintFlags(policy_of_conf.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
         enter_link.setPaintFlags(enter_link.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
@@ -68,5 +76,16 @@ public class Registration extends AppCompatActivity {
             }
         });
 
+    }
+
+    public boolean validateForm(EditText password_create,EditText repeat_pass){
+
+        if (!password_create.getText().toString().equals(repeat_pass.getText().toString())) {
+            Toast.makeText(Registration.this, "Пароли не совпали", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else{
+            return true;
+        }
     }
 }
